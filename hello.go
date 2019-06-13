204504b244f2f5b3cb58b96df6c3a2b0645ed425
Keith Barrette <etterrab@gmail.com>
2019-06-13 14:20:16 -0400

    Add dependencies

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/prometheus/client_golang/prometheus"
+import "bitbucket.org/zombiezen/gopdf/pdf"
+import "go.uber.org/multierr"
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
